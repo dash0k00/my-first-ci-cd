45324d3d24a435c10e2f8d54668fbf49a530238f
KirillovaDaria <dash0_k00@mail.ru>
2025-10-08 16:36:37 +0300

    feat: add flake8 code style checking

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import  Flask, request
+from flask import Flask, request
+
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
@@ -10,6 +12,6 @@ def hello_world():
         user_id = 1
     return f'<h1>Hello, user #{user_id}!</h1>'
 
+
 if __name__ == '__main__':
     app.run(debug=True)
- 
