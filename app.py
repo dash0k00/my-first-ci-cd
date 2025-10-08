9f102d6b0911d6e081dffca63df72bef89aa4ab4
KirillovaDaria <dash0_k00@mail.ru>
2025-10-08 17:04:39 +0300

    feat: add flake8 code style checking

--- app.py
+++ app.py
@@ -1,17 +1,9 @@
-from flask import Flask, request
+import pytest
+from app import app
 
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    try:
-        user_id = int(request.args.get('id', 1))
-    except ValueError:
-        user_id = 1
-    return f'<h1>Hello, user #{user_id}!</h1>'
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+def test_hello_world():
+    with app.test_client() as client:
+        response = client.get('/')
+        assert response.status_code == 200
+        assert b'Hello' in response.data
